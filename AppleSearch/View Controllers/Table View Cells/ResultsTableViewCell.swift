27efa2ebfb89306ea923448ece9125c428e58d03
Michael R.F <mrflowers22@uwalumni.com>
2019-10-03 11:41:21 -0600

    completed the storyboard

---
+++ AppleSearch/View Controllers/Table View Cells/ResultsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultsTableViewCell.swift
+//  AppleSearch
+//
+//  Created by Michael Flowers on 10/3/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
